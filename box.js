146fa5604e8ab3fc12d53f5f626e73e67364f099
Amograo <76100684+Amograo@users.noreply.github.com>
2021-01-08 18:06:49 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,26 @@
+class Box{
+
+    constructor(x,y,width,height){
+        var options = {
+            restitution: 1.0,
+            friction: 1.0,
+            density: 1.0,
+        
+          }
+        
+          this.body = Bodies.rectangle(x,y,width,height,options);
+          this.height = height
+          this.width = width
+          World.add(world,this.body);
+    }
+
+    display(){
+        push()
+        fill("red")
+        translate(this.body.position.x,this.body.position.y)
+        rotate(this.body.angle)
+        rectMode(CENTER)
+        rect(0,0,this.width,this.height)
+        pop()
+    }
+}
